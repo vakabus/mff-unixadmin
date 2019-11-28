72352f86b88cb46f7fb3a7491d1bc4587246f54f
Vasek Sraier <git@vakabus.cz>
2019-11-28 17:44:23 +0100

    updated deploy script to regenerate disks

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,26 @@ reboot() {
 	ssh unixadmin "killall -SIGINT supervisord; /home/sraierv/miniconda3/bin/supervisord"
 }
 
+shutdown() {
+	ssh unixadmin "killall -SIGINT supervisord"
+}
+
+boot() {
+	ssh unixadmin "/home/sraierv/miniconda3/bin/supervisord"
+}
+
+recreate_disks() {
+	images="$(find _build -name  "*.cpio.gz" | sed 's#_build/##;s/-.*//' | tr '\n' ' ')"
+	ssh unixadmin <<EOL
+rm -rf _disks
+mkdir -p _disks
+cd _disks
+for img in $images; do
+	qemu-img create \$img 8G
+done
+EOL
+}
+
 upload() {
 	rsync -r --progress _build unixadmin:
 	rsync -r --progress supervisord.conf unixadmin:supervisord.conf
@@ -11,8 +31,11 @@ upload() {
 
 
 
+
 time upload
-reboot
+shutdown
+recreate_disks
+boot
 
 echo
 echo "RAM disks uploaded and systems rebooted!"
